83d7f93d9717a052d5111928ef4467e10445b477
68fpjc <32516363+68fpjc@users.noreply.github.com>
2025-10-20 14:33:46 +0000

    Add support for VS Code Dev Containers

---
+++ .devcontainer/oncreated.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+set -e
+
+ARCH=$(dpkg --print-architecture)
+
+SERVER_VERSION=10.10.7
+SERVER_FILE=jellyfin_${SERVER_VERSION}-${ARCH}.tar.gz
+FFMPEG_VERSION=7.1.2-3
+FFMPEG_FILE=jellyfin-ffmpeg7_${FFMPEG_VERSION}-bookworm_${ARCH}.deb
+
+# Install Jellyfin Server
+curl -L https://repo.jellyfin.org/files/server/linux/stable/v${SERVER_VERSION}/${ARCH}/${SERVER_FILE} | sudo tar zxv -C /opt
+
+# Install Jellyfin FFmpeg 7.x
+curl -LO https://repo.jellyfin.org/files/ffmpeg/debian/7.x/${FFMPEG_VERSION}/${ARCH}/${FFMPEG_FILE}
+sudo apt-get update
+sudo apt-get install -y ./${FFMPEG_FILE}
+rm ${FFMPEG_FILE}
